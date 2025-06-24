8f534d29811a175085f2f137e5e27570d18687db
Deepak-Porwal-code <Porwal99deepak@gmail.com>
2025-06-24 13:23:28 +0530

    Update prompt.middleware.js

--- Backend/middleware/prompt.middleware.js
+++ Backend/middleware/prompt.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import config from "../config.js";
+import config from "../config";
 
 function userMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
